61cc986c30ab28238768a3b783c04ae37d45260b
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-19 15:06:49 -0300

    Added ingredient entity and related to recipe.

--- src/main/java/br/com/vizone/spring5recipeapp/domain/Recipe.java
+++ src/main/java/br/com/vizone/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package br.com.vizone.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -15,6 +16,9 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
@@ -100,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
