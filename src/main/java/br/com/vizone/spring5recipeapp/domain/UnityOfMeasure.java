4841068eb9fa6aef6b09ed2c99a7bd4e82d6d5a8
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-19 15:33:05 -0300

    Added Unity of Measure entity and related to ingredient.

---
+++ src/main/java/br/com/vizone/spring5recipeapp/domain/UnityOfMeasure.java
@@ -0,0 +1,30 @@
+package br.com.vizone.spring5recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnityOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
