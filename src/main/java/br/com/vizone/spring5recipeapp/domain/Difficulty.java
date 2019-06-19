b3b7bb467d409350d4b67d81bc1f27ca6dc99a26
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-19 16:01:53 -0300

    Added Enum difficulty.

---
+++ src/main/java/br/com/vizone/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package br.com.vizone.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY, MODERATE, HARD
+}
--- src/main/java/br/com/vizone/spring5recipeapp/domain/Recipe.java
+++ src/main/java/br/com/vizone/spring5recipeapp/domain/Recipe.java
@@ -23,6 +23,9 @@ public class Recipe {
     @Lob
     private Byte[] image;
 
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
 
@@ -90,6 +93,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     public Byte[] getImage() {
         return image;
     }
@@ -98,19 +109,19 @@ public class Recipe {
         this.image = image;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
-    }
-
-    public Set<Ingredient> getIngredients() {
-        return ingredients;
-    }
-
-    public void setIngredients(Set<Ingredient> ingredients) {
-        this.ingredients = ingredients;
     }
 }
