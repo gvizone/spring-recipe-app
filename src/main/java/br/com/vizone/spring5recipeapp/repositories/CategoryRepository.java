54b022631eafd91c063d7e8f099555735bc71c36
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-06-21 11:34:37 -0300

    Added repositories methods.

--- src/main/java/br/com/vizone/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/br/com/vizone/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package br.com.vizone.spring5recipeapp.repositories;
 import br.com.vizone.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
