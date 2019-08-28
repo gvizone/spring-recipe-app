c6d3d121a2d62c56ac5f5305875d19c446c4e952
Gabriel Vizone <gabriel.vizone@gmail.com>
2019-08-28 10:59:01 -0300

    commit for backup

--- src/main/java/br/com/vizone/spring5recipeapp/bootstrap/DataLoader.java
+++ src/main/java/br/com/vizone/spring5recipeapp/bootstrap/DataLoader.java
@@ -4,6 +4,7 @@ import br.com.vizone.spring5recipeapp.domain.*;
 import br.com.vizone.spring5recipeapp.repositories.CategoryRepository;
 import br.com.vizone.spring5recipeapp.repositories.RecipeRepository;
 import br.com.vizone.spring5recipeapp.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     private final CategoryRepository categoryRepository;
@@ -29,6 +31,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
     @Override
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
+        log.debug("Loading bootstrap data");
     }
 
     private List<Recipe> getRecipes() {
--- src/main/java/br/com/vizone/spring5recipeapp/services/RecipeServiceImpl.java
+++ src/main/java/br/com/vizone/spring5recipeapp/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package br.com.vizone.spring5recipeapp.services;
 
 import br.com.vizone.spring5recipeapp.domain.Recipe;
 import br.com.vizone.spring5recipeapp.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRespository;
@@ -17,6 +19,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in the service");
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRespository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
